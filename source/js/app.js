291f5f859988031e6f4cc180850bb0745712964a
Никита Ларичкин <nikita@MacBook-Pro-Nikita.local>
2019-12-22 19:10:53 +0300

    добавляет плавную прокрутку с библиотекой MoveTo

--- source/js/app.js
+++ source/js/app.js
@@ -1,6 +1,11 @@
 const closeBtn = document.querySelector(`#closeBtn`);
-const modalForm = document.querySelector(`.popup`);
 const callBtn = document.querySelector(`.contacts__button`);
+const modalForm = document.querySelector(`.popup`);
+const scrollBtn = document.querySelector(`.main-screen__scroll`);
+const moveTo = new MoveTo();
+const advantages = document.getElementById(`advantages`);
+const consultingBtn = document.querySelector(`.main-screen__button`);
+const consultingSection = document.getElementById(`consulting`);
 
 if(callBtn) {
   callBtn.addEventListener(`click`, (evt) => {
@@ -35,5 +40,19 @@ if (modalForm) {
       target.classList.remove(`popup--showing`);
       target.classList.add(`popup--closed`);
     };
+  });
+};
+
+if (scrollBtn && advantages) {
+  scrollBtn.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+    moveTo.move(advantages);
+  });
+};
+
+if (consultingBtn && consultingSection) {
+  consultingBtn.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+    moveTo.move(consultingSection);
   });
 };
